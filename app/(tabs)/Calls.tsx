639c0b13487dd287187f40242623fe1c1e2f8898
Phoenix <pbpham@wisc.edu>
2024-06-17 16:38:21 -0500

    added calls, people, reminder, stats pages. Deleted default react expo code.

---
+++ app/(tabs)/Calls.tsx
@@ -0,0 +1,31 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Image, Platform } from 'react-native';
+
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { ThemeProvider } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function callScreen() {
+  return (
+    <SafeAreaView>
+      <ThemedText>Testing Call SCreen</ThemedText>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
---
+++ app/(tabs)/People.tsx
@@ -0,0 +1,16 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function PeopleScreen() {
+       return (
+        <SafeAreaView>
+             <ThemedText type= 'title'> Testing People Screen </ThemedText>
+        </SafeAreaView>
+
+       )
+
+    
+
+}
