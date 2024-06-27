aef90723750b8f65dc6d96e109c71a6872fc18ae
Phoenix <pbpham@wisc.edu>
2024-06-27 17:54:51 -0500

    added a loadDataBase() and SQLiteProvider to initialize the db so we can access it in any file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,48 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import homeScreen from './(tabs)';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import homeScreen from "./(tabs)";
+import { SQLiteProvider } from "expo-sqlite";
+import { connectToDatabase, createTables } from "./db/db";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+
 export default function RootLayout() {
+
+  const loadData = useCallback(async () => {
+    try {
+      const db = await connectToDatabase();
+      await createTables(db);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
+    loadData()
+  },[loadData])
+
+  useEffect(() => {
+
+   
     if (loaded) {
       SplashScreen.hideAsync();
     }
@@ -28,15 +53,26 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index"  options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" options={{ headerShown: false }}/>
-        <Stack.Screen name="createGroupModal" options={{ headerShown: false }}/>
-        <Stack.Screen name="addMemberScreen" options={{ headerShown: false }}/>
-        <Stack.Screen name="addMemberManualScreen" options={{ headerShown: false }}/>
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SQLiteProvider databaseName="InTouchDB_1">
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="createGroupModal"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="addMemberScreen"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="addMemberManualScreen"
+            options={{ headerShown: false }}
+          />
+        </Stack>
+      </SQLiteProvider>
     </ThemeProvider>
   );
 }
