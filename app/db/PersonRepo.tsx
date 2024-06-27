f849b5f259f29dd44685795e8410eccb0b76576b
Phoenix <pbpham@wisc.edu>
2024-06-27 17:23:11 -0500

    added a updatePerson()

--- app/db/PersonRepo.tsx
+++ app/db/PersonRepo.tsx
@@ -28,12 +28,34 @@ export const addPerson = async (db: SQLite.SQLiteDatabase, person:Person) =>{
 
 }
 
+export const updatePerson = async (db: SQLite.SQLiteDatabase, updatedPerson: Person) => {
+    const statement = await db.prepareAsync(`
+        UPDATE person 
+        SET firstName = ?, lastName = ?, phoneNumber = ?
+        WHERE person_id = ?
+        `);
+
+    const value: string[] = [updatedPerson.firstName, updatedPerson.lastName, updatedPerson.phoneNumber, '1']
+    
+    try {
+        return await statement.executeAsync(value)
+    } catch (error) {
+        console.error(error)
+        throw Error("Failed to update person")
+    } finally {
+        // console.log("finalize updatePerson async")
+        statement.finalizeAsync()
+    }
+
+
+}
+
 export const getAllPersons = async (db: SQLite.SQLiteDatabase) =>{
 
     const people = await db.getAllAsync(`SELECT * FROM person`)
     // const people 
-    console.log("All persons in person", people)
+    // console.log("All persons in person", people)
 
-    return null;
+    return people;
 }
 
