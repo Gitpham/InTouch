1c74e189b05e7f7d3429a9d555203b1dc5ce60f5
Tonio Ermakoff <termakoff@gmail.com>
2024-06-30 15:43:49 -0500

    homescreen now displays list of user's groups, UI still needs to be refined

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { ThemedText } from "@/components/ThemedText";
-import { useState } from "react";
+import React, { useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { BottomSheet, Button, Dialog, ListItem } from '@rneui/themed';
@@ -14,7 +14,9 @@ export default function homeScreen() {
 
     return <SafeAreaView style = {styles.stepContainer}>
         <View style = {styles.centeredView}><ThemedText style = {styles.title} type = "title">My Groups</ThemedText></View>
-
+        <FlatList data = {groupList}
+          renderItem = {({item}) => <ListItem bottomDivider><ListItem.Title>{item.name}</ListItem.Title></ListItem>}
+        />
         <Button buttonStyle = {styles.button}
         titleStyle = {styles.title}
         title = {"+Add Group"}
@@ -34,7 +36,11 @@ const styles= StyleSheet.create({
     },
     title: {
       color: "black",
-   },
+    },
+    items: {
+      color: "black",
+      margin: 25
+    },
     stepContainer: {
       flex: 1,
       backgroundColor: 'white',
