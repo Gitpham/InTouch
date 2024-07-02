90510c4ed844ded18fdde665c53e6ab700209abd
Phoenix <pbpham@wisc.edu>
2024-07-02 13:33:21 -0500

    deleted Group.tsx

--- app/Group.tsx
+++
@@ -1,4 +0,0 @@
-// Group to be saved
-interface Group { name: string, members: Member[], schedule: string, typeOfCall: string}
-
-interface Member { firstName: string, lastName: string, number: string}
