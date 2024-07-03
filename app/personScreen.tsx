e13d1c76cb90fe39c0a9bf660f94c613053be6d4
Phoenix <pbpham@wisc.edu>
2024-07-03 15:55:54 -0500

    added some fcns to get person from peopleList

--- app/personScreen.tsx
+++ app/personScreen.tsx
@@ -2,21 +2,28 @@ import { ThemedText } from "@/components/ThemedText";
 import { Person } from "@/constants/types";
 import { Card } from "@rneui/themed";
 import { useLocalSearchParams } from "expo-router";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { InTouchContext } from "@/context/InTouchContext";
 
 export default function PersonScreen() {
 
+  const {peopleList} = useContext(InTouchContext)
   const localParams = useLocalSearchParams();
+  let person: Person;
+
   useEffect(() => {
-    console.log("local params", localParams)
+    const personId: number = Number(localParams.id)
+    console.log(peopleList[personId -1])
+    person = peopleList[personId -1];
+
   }, [])
+
        return (
         <SafeAreaView>
              <ThemedText type= 'title'> Person Screen </ThemedText>
              <Card>
-               <Card.Title>Person: </Card.Title>
+               <Card.Title>Person:  </Card.Title>
              </Card>
         </SafeAreaView>
 
