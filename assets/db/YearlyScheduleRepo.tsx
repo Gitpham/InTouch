a3514f72ff8189819e8ac21b7a98105b18344be8
Phoenix <pbpham@wisc.edu>
2024-07-25 18:27:09 -0500

    added upload, get, delete methods to MonthlyScheduleRepo

--- assets/db/YearlyScheduleRepo.tsx
+++ assets/db/YearlyScheduleRepo.tsx
@@ -9,25 +9,25 @@ import * as SQLite from "expo-sqlite";
  * @param bid 
  * @returns 
  */
-export async function uploadMonthlyScheduleDB(db: SQLite.SQLiteDatabase, time: string, dayOfWeek: number, weekOfMonth: number, nid: string, bid: number){
+export async function uploadYearlyScheduleDB(db: SQLite.SQLiteDatabase, time: string, date: string, nid: string, bid: number){
     const statement =
-    await db.prepareAsync(`INSERT INTO monthlySchedule (time, dayOfWeek, weekOfMonth, notification_id, bond_id)
-         VALUES (?, ?, ?, ?, ?);`);
+    await db.prepareAsync(`INSERT INTO yearlySchedule (time, date, notification_id, bond_id)
+         VALUES (?, ?, ?, ?);`);
 
-    const value: string[] = [time, dayOfWeek.toString(), weekOfMonth.toString(), nid, bid.toString()];
+    const value: string[] = [time, date.toString(), nid, bid.toString()];
   try {
     return await statement.executeAsync(value);
   } catch (error) {
     console.error(error);
-    throw Error("uploadMonthlyScheduleDB() failed");
+    throw Error("uploadYearlyScheduleDB() failed");
   } finally {
     statement.finalizeAsync();
   }
 }
 
-export const deleteMonthlyScheduleDB = async (db: SQLite.SQLiteDatabase, bid: number) => {
+export const deleteYearlyScheduleDB = async (db: SQLite.SQLiteDatabase, bid: number) => {
     const statement = await db.prepareAsync(`
-         DELETE FROM monthlySchedule
+         DELETE FROM yearlySchedule
         WHERE bond_id = ?
           `);
   
@@ -37,7 +37,7 @@ export const deleteMonthlyScheduleDB = async (db: SQLite.SQLiteDatabase, bid: nu
           return await statement.executeAsync(value)
       } catch (error) {
           console.error(error)
-          throw Error("deleteMonthlyScheduleDB() Failed")
+          throw Error("deleteYearlyScheduleDB() Failed")
       } finally {
           // console.log("finalize updatePerson async")
           statement.finalizeAsync()
@@ -45,14 +45,14 @@ export const deleteMonthlyScheduleDB = async (db: SQLite.SQLiteDatabase, bid: nu
     }
 
     export const getMonthlyScheduleDB = async (db: SQLite.SQLiteDatabase, bid: number) => {
-        const statement = await db.prepareAsync(`SELECT * FROM monthlySchedule WHERE bond_id = ?;`)
+        const statement = await db.prepareAsync(`SELECT * FROM yearlySchedule WHERE bond_id = ?;`)
         const value: string[] = [bid.toString()]
         try {
             const result = await statement.executeAsync(value);
             return await result.getFirstAsync();
         } catch (error) {
             console.error(error)
-            throw Error("getMonthlySchedule() failed")
+            throw Error("getYearlySchedule() failed")
         }
     }
     
