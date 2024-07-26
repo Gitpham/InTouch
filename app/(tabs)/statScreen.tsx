e3e3db36d7b3c6730348f946a05ffa24cc422236
Phoenix <pbpham@wisc.edu>
2024-07-26 12:12:29 -0500

    removed notification stuff from statScreen

--- app/(tabs)/statScreen.tsx
+++ app/(tabs)/statScreen.tsx
@@ -1,72 +1,13 @@
-import * as Notifications from "expo-notifications";
-import { registerForPushNotificationsAsync } from "@/context/NoticationUtil";
-import { useState, useRef, useEffect } from "react";
-import { Platform, View, Button, Text } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-Notifications.setNotificationHandler({
-     handleNotification: async () => ({
-       shouldShowAlert: true,
-       shouldPlaySound: false,
-       shouldSetBadge: false,
-     }),
-   });
-   
-   export default function statScreen() {
-     const [expoPushToken, setExpoPushToken] = useState('');
-     const [channels, setChannels] = useState<Notifications.NotificationChannel[]>([]);
-     const [notification, setNotification] = useState<Notifications.Notification | undefined>(
-       undefined
-     );
-     const notificationListener = useRef<Notifications.Subscription>();
-     const responseListener = useRef<Notifications.Subscription>();
-   
-     useEffect(() => {
-       registerForPushNotificationsAsync().then(token => token && setExpoPushToken(token));
-   
-       if (Platform.OS === 'android') {
-         Notifications.getNotificationChannelsAsync().then(value => setChannels(value ?? []));
-       }
-       notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-         setNotification(notification);
-       });
-   
-       responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-         console.log(response);
-       });
-   
-       return () => {
-         notificationListener.current &&
-           Notifications.removeNotificationSubscription(notificationListener.current);
-         responseListener.current &&
-           Notifications.removeNotificationSubscription(responseListener.current);
-       };
-     }, []);
-   
-     return (
-       <View
-         style={{
-           flex: 1,
-           alignItems: 'center',
-           justifyContent: 'space-around',
-         }}>
-         <Text>Your expo push token: {expoPushToken}</Text>
-         <Text>{`Channels: ${JSON.stringify(
-           channels.map(c => c.id),
-           null,
-           2
-         )}`}</Text>
-         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-           <Text>Title: {notification && notification.request.content.title} </Text>
-           <Text>Body: {notification && notification.request.content.body}</Text>
-           <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-         </View>
-         <Button
-           title="Press to schedule a notification"
-           onPress={async () => {
-             await schedulePushNotification();
-           }}
-         />
-       </View>
-     );
-   }
+export default function statScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Nothing Here Yet</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
