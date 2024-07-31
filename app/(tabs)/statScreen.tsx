9566040b12a6eb5f6acc1974f6c3f9e494e3a276
Phoenix <pbpham@wisc.edu>
2024-07-30 21:01:31 -0500

    Notifications are scheduled but do not show up when they should

--- app/(tabs)/statScreen.tsx
+++ app/(tabs)/statScreen.tsx
@@ -4,21 +4,34 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StandardButton } from "@/components/ButtonStandard";
 import { getAllSchedules, getScheduleOfBond } from "@/assets/db/ScheduleRepo";
 import { useSQLiteContext } from "expo-sqlite";
+import { cancelAllNotifications, getAllScheduledNotifications } from "@/context/NotificationUtils";
 
 export default function statScreen() {
   const db = useSQLiteContext();
 
   async function onDisplaySchedules() {
     const s = await getAllSchedules(db)
-    console.log(s);
+    console.log("schedules in db: ", s);
   }
 
+  async function onDisplayNotifications() {
+    console.log("getAllScheduledNotifications(): " , getAllScheduledNotifications())
+  }
+
+
+  async function clearScheduledNotifications() {
+    await cancelAllNotifications()
+  }
 
   return (
     <SafeAreaView>
       <View>
         <Text>Nothing Here Yet</Text>
         <StandardButton title="Display Schedules from DB" onPress={onDisplaySchedules}></StandardButton>
+        <StandardButton title="Display scheudleNotificaions" onPress={onDisplayNotifications}></StandardButton>
+        <StandardButton title="cancel all Notifications" onPress={clearScheduledNotifications}></StandardButton>
+
+
       </View>
     </SafeAreaView>
   );
