887ca83bdaaaf1e1d8d44394a12ae324cb6730eb
Phoenix <pbpham@wisc.edu>
2024-08-02 11:52:14 -0500

    rerannged montly scheudle picker so its vertical

--- components/MonthlySchedulePicker.tsx
+++ components/MonthlySchedulePicker.tsx
@@ -5,7 +5,7 @@ import { StandardButton } from "./ButtonStandard";
 import DateTimePicker, {
   DateTimePickerEvent,
 } from "@react-native-community/datetimepicker";
-import { DayOfMonth } from "@/constants/types";
+import { DateAndTime, DayOfMonth } from "@/constants/types";
 import { Picker } from "@react-native-picker/picker";
 import {
   convertNumberToOrdinal,
@@ -81,10 +81,24 @@ export default function MonthlySchedulePicker({
   }
 
   function onAddDayOfMonth() {
-    const day: DayOfMonth = {
-      weekOfMonth: selectedWeekOfMonth as number,
-      dayOfWeek: selectedDayOfWeek as number,
-      time: monthlyTime,
+    if(onOnThePressed){
+      const day: DayOfMonth = {
+        weekOfMonth: selectedWeekOfMonth as number,
+        dayOfWeek: selectedDayOfWeek as number,
+        time: monthlyTime,
+      };
+  
+      const newDay = new Set([day]);
+  
+      if (!monthlySet.has(day)) {
+        changeMonthlySet(new Set<DayOfMonth>([...monthlySet, ...newDay]));
+      }
+      return;
+    }
+
+    const day: DateAndTime = {
+      date: selectedMonthDate,
+      time: selectedMonthDateTime
     };
 
     const newDay = new Set([day]);
@@ -92,7 +106,9 @@ export default function MonthlySchedulePicker({
     if (!monthlySet.has(day)) {
       changeMonthlySet(new Set<DayOfMonth>([...monthlySet, ...newDay]));
     }
-    return;
+
+
+    
   }
 
   function onOnThePressed() {
@@ -108,41 +124,48 @@ export default function MonthlySchedulePicker({
   function displayOnThePicker() {
 
     return (
-      <View style={styles.centeredView}>
-      <View style={styles.rowOrientation }>
-        <View style={{ flex: 0.5, flexDirection: "column" }}>
+      <View style={styles.stepContainer}>
+      {/* <View style={styles.rowOrientation }> */}
+        <View 
+        // style={{ flex: 0.5, flexDirection: "column" }}
+        >
           <Picker
             selectedValue={selectedWeekOfMonth}
             onValueChange={(itemValue, itemIndex) => {
               changeSelectedWeekOfMonth(itemValue);
             }}
           >
-            <Picker.Item label="1st" value="1" />
-            <Picker.Item label="2nd" value="2" />
-            <Picker.Item label="3rd" value="3" />
-            <Picker.Item label="4th" value="4" />
+            <Picker.Item label="First" value="1" />
+            <Picker.Item label="Second" value="2" />
+            <Picker.Item label="Third" value="3" />
+            <Picker.Item label="Fourth" value="4" />
           </Picker>
         </View>
 
-        <View style={{ flex: 0.5, flexDirection: "column" }}>
+        <View 
+        // style={{ flex: 0.5, flexDirection: "column" }}
+        >
           <Picker
             selectedValue={selectedDayOfWeek}
             onValueChange={(itemValue, itemIndex) =>
               changeSelectedDayOfWeek(itemValue)
             }
           >
-            <Picker.Item label="Sun." value="1" />
-            <Picker.Item label="Mon." value="2" />
-            <Picker.Item label="Tues." value="3" />
-            <Picker.Item label="Weds." value="4" />
-            <Picker.Item label="Thurs." value="5" />
-            <Picker.Item label="Fri." value="6" />
-            <Picker.Item label="Sat." value="7" />
+            <Picker.Item label="Sunday" value="1" />
+            <Picker.Item label="Monday" value="2" />
+            <Picker.Item label="Tuesday" value="3" />
+            <Picker.Item label="Wednesday" value="4" />
+            <Picker.Item label="Thursday" value="5" />
+            <Picker.Item label="Friday" value="6" />
+            <Picker.Item label="Saturday" value="7" />
           </Picker>
-        </View>
+        {/* </View> */}
       </View>
+      <View style={styles.centeredView}>
       <ThemedText style={styles.title}> Of the month at </ThemedText>
-      <View>
+
+      </View>
+      <View style={styles.centeredView}>
         <DateTimePicker
           value={monthlyTime}
           mode="time"
@@ -155,18 +178,82 @@ export default function MonthlySchedulePicker({
     
   }
 
+  // Function to get the first date of the month
+function getFirstDateOfMonth(date: Date) {
+  // Create a new Date object with the same year and month, but set the day to 1
+  return new Date(date.getFullYear(), date.getMonth(), 1);
+}
+
+function getLastDateOfMonth(date: Date) {
+  // Create a new Date object with the same year and month, but set the day to 1
+  return new Date(date.getFullYear(), date.getMonth(), 31);
+}
+
   function displayMonthlyDatePicker() {
 
     return (
       <View>
-        <DateTimePicker
-        minimumDate={new Date()}
+        <View style={styles.centeredView}>
+
+      <Text style={styles.title}>{selectedMonthDate.getDate()} OF THE MONTH</Text>
+      </View>
+      <Picker
+            selectedValue={selectedWeekOfMonth}
+            onValueChange={(itemValue, itemIndex) => {
+              changeSelectedWeekOfMonth(itemValue);
+            }}
+          >
+            <Picker.Item label="1" value="1" />
+            <Picker.Item label="2" value="2" />
+            <Picker.Item label="3" value="3" />
+            <Picker.Item label="4" value="4" />
+            <Picker.Item label="5" value="5" />
+            <Picker.Item label="6" value="6" />
+            <Picker.Item label="7" value="7" />
+            <Picker.Item label="8" value="8" />
+            <Picker.Item label="9" value="9" />
+            <Picker.Item label="10" value="10" />
+            <Picker.Item label="11" value="11" />
+            <Picker.Item label="12" value="12" />
+            <Picker.Item label="13" value="13" />
+            <Picker.Item label="14" value="14" />
+            <Picker.Item label="15" value="15" />
+            <Picker.Item label="16" value="16" />
+            <Picker.Item label="17" value="17" />
+            <Picker.Item label="18" value="18" />
+            <Picker.Item label="19" value="19" />
+            <Picker.Item label="20" value="20" />
+            <Picker.Item label="21" value="21" />
+            <Picker.Item label="22" value="22" />
+            <Picker.Item label="23" value="23" />
+            <Picker.Item label="24" value="24" />
+            <Picker.Item label="25" value="25" />
+            <Picker.Item label="26" value="26" />
+            <Picker.Item label="27" value="27" />
+            <Picker.Item label="28" value="28" />
+            <Picker.Item label="29" value="29" />
+            <Picker.Item label="30" value="30" />
+            <Picker.Item label="31" value="31" />
+          </Picker>
+
+        {/* <DateTimePicker style={{alignSelf:"flex-start"}}
+        minimumDate={getFirstDateOfMonth(new Date())}
+        maximumDate={getLastDateOfMonth(new Date())}
       value={selectedMonthDate}
       onChange={(e, d) => {
           changeSelectedMonthDate(d)
       }}
       >
-      </DateTimePicker>
+      </DateTimePicker> */}
+
+      <View style={styles.centeredView}>
+      <ThemedText style={styles.title}> at </ThemedText>
+        <DateTimePicker
+          value={selectedMonthDateTime}
+          mode="time"
+          onChange={(e, d) => changeSelectedMonthDateTime(d)}
+        ></DateTimePicker>
+      </View>
   </View>
     )
 
@@ -176,21 +263,27 @@ export default function MonthlySchedulePicker({
     <ScrollView style={{ padding: 10 }}>
     
       <Card>
+        <View style={styles.centeredView}>
+          <ThemedText darkColor="black" style={styles.title}>REPEAT</ThemedText>
+        </View>
       <View style={styles.rowOrientation}>
         <CheckBox checked={onTheIsChecked} onPress={onOnThePressed}title="On the" />
-        <CheckBox checked={eachIsChecked} onPress={onEachPressed} title="Each" />
+        <CheckBox checked={eachIsChecked} onPress={onEachPressed} title="Every" />
       </View>
       {
         onTheIsChecked ? 
         displayOnThePicker() :
         displayMonthlyDatePicker()
       }
+      <StandardButton title="Add" onPress={onAddDayOfMonth}></StandardButton>
      
       </Card>
 
-      <StandardButton title="Add" onPress={onAddDayOfMonth}></StandardButton>
       <Card>
+        <View style={styles.centeredView}>
         <Text style={styles.title}>Current Monthly Schedule: </Text>
+
+        </View>
         {displayMonthlySet()}
         <StandardButton
           title="Clear current schedule"
