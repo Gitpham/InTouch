1766fdd17794d1fc6babbbf26985c68a51d06fc1
Tonio Ermakoff <termakoff@gmail.com>
2024-08-06 11:36:40 -0500

    picking up Phoenix slack

--- app/addMemberScreen.tsx
+++ app/addMemberScreen.tsx
@@ -2,7 +2,7 @@ import { ThemedText } from "@/components/ThemedText";
 import { useContext, useEffect, useState } from "react";
 import { Alert, FlatList, Pressable, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {  ListItem } from "@rneui/themed";
+import { ListItem, Card } from "@rneui/themed";
 import {  View } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
 import * as Contacts from "expo-contacts";
@@ -15,7 +15,7 @@ import React from "react";
 
 export default function addMemberScreen() {
   const { createPerson, addTempBondMember, generatePersonId , tempBondMembers, peopleList, bondPersonMap, createBondMember, clearTempBondMembers } = useContext(InTouchContext);
-  const [ refresh, setRefresh] = useState(false)
+  const [ refresh, setRefresh ] = useState(false)
   const [memberFirstName, memFirstNameChange] = useState("");
   const [memberLastName, memLastNameChange] = useState("");
   const [memberNumber, memNumberChange] = useState("");
@@ -45,9 +45,9 @@ export default function addMemberScreen() {
       }
     });
 
-    console.log(peopleToShow)
+
     setMembersToShow(peopleToShow)
-  }, [, tempBondMembers, peopleList]);
+  }, [tempBondMembers, peopleList]);
   
   const group_screen = +localParams.group_screen;
 
@@ -61,29 +61,25 @@ export default function addMemberScreen() {
         // Generate unique person id
         const personID = generatePersonId();
 
+        const lastName = ""
+
+        if (person.lastName) {
+          lastName = person.lastName
+        }
+
         const newContact: Person = {
           firstName: person?.firstName as string,
-          lastName: person?.lastName as string,
+          lastName: lastName,
           phoneNumber: person?.phoneNumbers?.[0]?.number as string,
           person_id: undefined,
         };
         await createPerson(newContact);
-        
-        if (bondId !== -1) {
-          console.log("addTBondMember")
-          addTempBondMember(personID);
-          }
       } else {
         Alert.alert("unable to add from contacts");
       }
     }
   }
   const addBondMember = ({ item }: { item: Person }) => {
-    // if (!tempBondMembers.has(item.person_id as number)) {
-    //   const bond_members = bondPersonMap.get(bondId);
-    //   if (bond_members && bond_members.has(item.person_id as number)) {
-    //     return null;
-    //   }
       return (
         <ListItem bottomDivider>
           <Pressable onPress={() => { addTempBondMember(item.person_id as number); setRefresh((oldValue) => !oldValue); }}>
@@ -118,10 +114,8 @@ export default function addMemberScreen() {
       <StandardButton
         title="Create New Contact"
         onPress={() => {
-          // router.navigate({pathname: "./addMemberManualScreen", params: {bond_id: localParams.bond_id}});
           const newVisible = !isVisible;
           setIsVisible(newVisible);
-          console.log(membersToShow)
         }}
       />
       
@@ -146,12 +140,12 @@ export default function addMemberScreen() {
         onPress={importFromContacts}
       />
 
+      <Card>
+      <Card.Title>Choose From inTouch Contacts</Card.Title>
       <View style={styles.centeredView}>
         {((bondId !== -1) && (peopleList.length !== 0)) ?  (
-        <>
-        <ThemedText type="subtitle" style={styles.title}>
-          Choose From inTouch Contacts
-        </ThemedText>
+
+          
       
       <FlatList
         data={membersToShow}
@@ -159,8 +153,9 @@ export default function addMemberScreen() {
         renderItem={addBondMember}
         keyExtractor={(item) => item.person_id.toString()}
       />
-      </>) :  null}
+      ) :  null}
       </View>
+      </Card>
 
       <StandardButton
         title="Save"
